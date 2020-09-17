0aac6b0465994cc9a59a85d3189378c60d4e2a28
WhiteAndrew91 <jimmycane991@gmail.com>
2020-09-17 17:01:24 +0300

    MusicApp

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="titleBanner">
+      
+          <p>The app is under development</p>
+
+          <a href="http://main.emdev.ro">Back</a>
+        </div>
+      </div>
+   
+  );
+}
+
+export default App;
